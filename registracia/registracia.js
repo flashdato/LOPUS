f4cfbdc2e54fddd216883089b2480b3444f68cdb
flash <flashdato@gmail.com>
2021-01-07 16:12:27 +0400

    first commit

---
+++ registracia/registracia.js
@@ -0,0 +1,26 @@
+const password = document.getElementById('password')
+const email = document.getElementById('email').value
+const password2 = document.getElementById('password2')
+const form = document.getElementById('form')
+const errorElement = document.getElementById('error')
+const pattern = /^[^ ]+@[^ ]+\.[ a-z ]{2,3}$/
+form.addEventListener('submit', (e) => {
+  let messages = []
+  if (password.value.length < 6) {
+    messages.push('პაროლი უნდა იყოს მინიმუმ 6 სიმბოლო')
+  }
+  if (password.value.length >= 20) {
+    messages.push('პაროლი არ შეიძლება იყოს 20 სიმბოლოზე მეტი')
+  }
+  if (password.value !== password2.value) {
+    messages.push('პაროლები  არ ემთხვევა')
+  }
+  if(!email.match(pattern))
+  {
+    messages.push('ელფოსტა არასწორია')
+  }
+  if (messages.length > 0) {
+    e.preventDefault()
+    errorElement.innerText = messages.join(', ')
+  }
+})
